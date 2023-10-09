d6d375370e9167cc69cae06c76bd68b8d6d819b5
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-09 22:15:45 +0530

    added footer

---
+++ my-app/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <nav>
+        <div class="nav-links-container">
+          <ul class="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; 2023 Shantanu Gahtori. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default footer
