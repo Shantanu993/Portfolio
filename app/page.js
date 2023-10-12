3c5b7d6854dd350d6e7d11a045cc8a89871d289d
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-12 23:38:44 +0530

    configured github calendar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import About from '@/components/About'
+import Github from '@/components/GithubCalendar'
 import Navbar from '@/components/Navbar'
 import Profile from '@/components/Profile'
 import Image from 'next/image'
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
@@ -9,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Profile/>
       <About/>
+      <Github/>
     </>
   )
 }
